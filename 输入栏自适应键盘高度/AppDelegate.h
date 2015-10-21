9a2393a585c19a023d69a624611307597da64f4c
liulianqi <liulianqi@liulianqidemacbook-pro.local>
2015-10-21 17:41:16 +0800

    Initial Commit

---
+++ 输入栏自适应键盘高度/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  输入栏自适应键盘高度
+//
+//  Created by liulianqi on 15/10/21.
+//  Copyright © 2015年 sunxianglong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 输入栏自适应键盘高度/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  输入栏自适应键盘高度
+//
+//  Created by liulianqi on 15/10/21.
+//  Copyright © 2015年 sunxianglong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
